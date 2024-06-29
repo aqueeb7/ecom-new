02be7f8db8847fecd668d755f1c0560b63564db0
Aqueeb7 <mdaqueeb3@gmail.com>
2024-06-29 13:10:54 +0530

    first commit

---
+++ service/user/routes.go
@@ -0,0 +1,73 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aqueeb7/ecom/service/auth"
+	"github.com/aqueeb7/ecom/types"
+	"github.com/aqueeb7/ecom/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+	// TODO: add more fields
+	store types.UserStore
+}
+
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRouter(router *mux.Router) {
+	router.HandleFunc("/login", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+	// TODO: implement login
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// TODO: get Json Payload
+	var payload types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+		return
+	}
+
+	// TODO: check if the user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with %s already exist", payload.Email))
+		return
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	// TODO: if it doesnt we create the new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
